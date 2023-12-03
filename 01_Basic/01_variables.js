587c6a078f3a330fa59f5817e9ed0a33dacd498b
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2023-12-03 12:39:54 +0000

    Variables of JS

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 1234
+let accountEmail = "akash@google.com"
+var accountPassword = "1826"
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountID, accountEmail, accountCity, accountPassword, accountState])
