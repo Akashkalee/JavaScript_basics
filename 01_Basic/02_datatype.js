99df4dc4682e53455c1cf36fe3d70fa253fa4a4a
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2023-12-04 20:45:44 +0000

    Datatype of JS

--- 01_Basic/02_datatype.js
+++ 01_Basic/02_datatype.js
@@ -1,18 +1,19 @@
-// javascript has 8 datatypes
-// 1. String
-// 2. Number
-// BigInt
-// Boolean
-// Undefined
-// Null
-// Symbol
-// Object
+/* javascript has 8 datatypes
 
-/*
+String
+ Number
+ BigInt
+ Boolean
+ Undefined
+ Null
+ Symbol
+ Object
+
 The object datatype can contain:
 1. An Object
 2. An array
 3. A date
+
 */
 
 let length = 14;
