a3505b21be1aa4e96c7f112e00397a2c2542cf50
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2023-12-04 20:14:12 +0000

    Datatypes

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,32 @@
+// javascript has 8 datatypes
+// 1. String
+// 2. Number
+// BigInt
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+/*
+The object datatype can contain:
+1. An Object
+2. An array
+3. A date
+*/
+
+let length = 14;
+
+let color = "yellow";
+
+let x = true;
+
+const person = {firstName:"John", lastNmae:"Swon"}
+
+const cars = ["Saab", "Volvo"];
+
+const date = new Date("2023-12-04")
+
+
+
+
