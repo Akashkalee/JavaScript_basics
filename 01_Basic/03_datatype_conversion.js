cc84a5f78f7703f6e6aa92c7de22ff48d977476e
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2023-12-09 13:39:05 +0000

    datatype converision in JS

---
+++ 01_Basic/03_datatype_conversion.js
@@ -0,0 +1,28 @@
+// conversion and operations
+
+// let score = "josh"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(isLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
