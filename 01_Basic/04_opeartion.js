6e6488327e86aff28fcff6076cf3e06e9356b3b6
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2024-01-10 10:57:41 +0000

    operation and comparison

---
+++ 01_Basic/04_opeartion.js
@@ -0,0 +1,13 @@
+// operations 
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+ 
